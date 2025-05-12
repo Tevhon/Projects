8385d6e51fc0f06860b88ecc909cf0081ce97157
Tevhon <151666120+Tevhon@users.noreply.github.com>
2025-05-12 21:30:07 +0530

    update

--- tic tac toe/script.js
+++ tic tac toe/script.js
@@ -1,3 +1,4 @@
+
 let boxes = document.querySelectorAll(".box");
 let rstBtn = document.querySelector(".reset-game");
 let newGame = document.querySelector(".new-game");
@@ -7,30 +8,30 @@ let msg = document.querySelector(".msg");
 let turn0 = true;
 
 const winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
 ];
 
-let resetGame = ()=>{
+let resetGame = () => {
     turn0 = true;
     enableBoxes();
     msgContainer.classList.add("hide");
 }
 
 
-boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{
-        if (turn0){
-            box.innerText ="0";
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turn0) {
+            box.innerText = "0";
             turn0 = false;
-        }else{
-            box.innerText ="X";
+        } else {
+            box.innerText = "X";
             turn0 = true;
         }
         box.disabled = true;
@@ -41,33 +42,33 @@ boxes.forEach((box)=>{
 
 
 
-const disableBoxes =() =>{
+const disableBoxes = () => {
     for (let box of boxes) {
         box.disabled = true;
     }
 }
 
-const enableBoxes =() =>{
+const enableBoxes = () => {
     for (let box of boxes) {
         box.disabled = false;
         box.innerText = "";
     }
 }
-const showWinner =(Winner) =>{
-    msg.innerText = `Congratulations, winner is ${ Winner}`;
+const showWinner = (Winner) => {
+    msg.innerText = `Congratulations, winner is ${Winner}`;
     msgContainer.classList.remove("hide");
     disableBoxes();
 }
 
-const checkWinner =() =>{
+const checkWinner = () => {
     for (let pattern of winPatterns) {
-       
-        let pos1val =  boxes[pattern[0]].innerText;
-        let pos2val =  boxes[pattern[1]].innerText;
-        let pos3val =  boxes[pattern[2]].innerText;
 
-        if(pos1val != "" && pos2val != "" && pos3val != ""){
-            if(pos1val === pos2val && pos2val=== pos3val){
+        let pos1val = boxes[pattern[0]].innerText;
+        let pos2val = boxes[pattern[1]].innerText;
+        let pos3val = boxes[pattern[2]].innerText;
+
+        if (pos1val != "" && pos2val != "" && pos3val != "") {
+            if (pos1val === pos2val && pos2val === pos3val) {
                 showWinner(pos1val)
             }
         }
